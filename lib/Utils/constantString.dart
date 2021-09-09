6007873f281f179b693ab92d38ad3e6c8b81dad4
amey00712 <amey00712@gmail.com>
2021-09-09 16:18:49 +0530

    fixes

--- lib/Utils/constantString.dart
+++ lib/Utils/constantString.dart
@@ -9,3 +9,4 @@ final String walletURL = 'wallet';
 final String cartRemoveURL = 'cart-remove';
 final String withdrawAmountURL = 'withdrow-amount';
 final String buyerOrderListURL = 'buyer-order-list';
+final String sellerOrderListURL = 'seller-order-list';
